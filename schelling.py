5703a8af2809739401e3475c365ff84a407d05df
Lucas <lucas.saner@gmail.com>
2026-01-12 23:10:27 -0300

    Finished setting up model population

--- schelling.py
+++ schelling.py
@@ -4,7 +4,7 @@ class SchellingAgent(mesa.Agent):
     def __init__(self,model,agent_type,unique_id):
         super().__init__(unique_id,model)
 
-        self.type = agent_type
+        self.agent_type = agent_type
     def step(self):
         pass
 
@@ -16,9 +16,25 @@ class SchellingModel(mesa.Model):
         self.height = height
         self.density = density
         self.homophily = homophily
-
+        self.grid = mesa.space.SingleGrid(width,height,torus = True)
+        self.schedule = mesa.time.RandomActivation(self)
+        for x in range (width):
+            for y in range (height):
+                if self.random.random() < self.density:
+                    if self.random.random() < self.minority:
+                        agent_type = 1
+                    else:
+                        agent_type = 0
+                        
+                    agent = SchellingAgent()
+                    self.grid.place_agent(agent,(x,y))
+                    self.schedule.add(agent)
+                        
+                        
     def step(self):
-        pass 
+        self.schedule.step()
+
+    
 
 
 
