c95a0542c8fbb7d79b280e84a850bb9ef27c355b
Lucas Saner <lucas.saner@gmail.com>
2026-02-28 14:29:21 -0300

    Fully implemented and integrated the Bond issuance method to the Firm class. This also required the _asset_value method to be added to the said class

--- supply_chain/supple_chain.py
+++ supply_chain/supple_chain.py
@@ -1,23 +1,27 @@
 import random
 
 class Firm(object):
-    def __init__(self, name, cash, inventory, market_hub, industry, margins):
+    def __init__(self, name, cash, inventory, market_hub, industry, debt, debt_limit):
         self.name = name
         self.cash = cash
         self.inventory = inventory
         self.industry = industry
         self.margins = random.uniform(0.02,0.50)
         market_hub.register_participant(self)
+        self.debt = 0
+        self.debt_limit = self.asset_value()
      
     def transact(self,other,item,amount):
         price = other.inventory[item]['price']
         total_price = price * amount
-        if self.cash < total_price:
+        if self.cash < total_price and self.debt + total_price - self.cash > self.debt_limit:
             print(f"{self.name} has insufficient funds to pay ${total_price:.2f}")
         if other.inventory[item]['quantity'] < amount:
             print(f"{other.name} cannot fulfill this order")
 
-        if self.cash >= total_price and amount <= other.inventory[item]['quantity']:
+        if (self.cash >= total_price or self.debt + total_price - self.cash <= self.debt_limit) and amount <= other.inventory[item]['quantity']:
+            if self.cash < total_price:
+                self._issue_bonds(total_price - self.cash)
             other.cash += total_price
             self.cash -=  total_price
             other.inventory[item]['quantity'] -= amount
@@ -26,6 +30,28 @@ class Firm(object):
             else:
                 self.inventory[item]= {'quantity': amount, 'price' : price }
             print(f"{self.name} paid ${total_price:.2f} for {amount} {item}")
+    
+    def _asset_value(self):
+        total_asset_value = 0
+        for item in self.inventory:
+            total_asset_value += self.inventory[item]['price'] * self.inventory[item]['quantity']
+        return total_asset_value
+
+    def _issue_bonds(self,face_value):
+        total_asset_value = self.cash + self.asset_value() 
+        debt_ratio = self.debt/total_asset_value
+
+        interest_rate = 0.02 + (debt_ratio * 0.18)
+        debt_incurred = face_value * (1 + interest_rate)
+
+        if self.debt + debt_incurred <= self.debt_limit:
+            self.cash += face_value
+            self.debt += debt_incurred
+            print(f"Issued ${face_value} of bonds at a %{interest_rate} yearly rate")
+        else:
+            print(f"Unable to issue more debt due to debt constraints")
+
+
 
 class Market(object):
     def __init__(self):
@@ -72,7 +98,9 @@ class TechFirm(Firm):
 class Miner(Firm):
     def _mine(self, amount,item):
         cost_to_mine = amount * 4
-        if self.cash >= cost_to_mine:
+        if self.cash >= cost_to_mine or (self.debt + cost_to_mine - self.cash) <= self.debt_limit:
+            if self.cash < cost_to_mine:
+                self.issue_bonds(cost_to_mine - self.cash)
             self.cash -= cost_to_mine
             if item in self.inventory:
                 self.inventory[item]['quantity'] += amount
